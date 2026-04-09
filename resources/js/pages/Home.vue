2338bfb9da2c4d10676fd3d83147444cd895200f
CSfrogy <lonelyguse@gmail.com>
2026-04-09 16:08:03 +0800

    First blueprint of frontend VUE

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <Head title="Main">
+    <link rel="preconnect" href="https://rsms.me/" />
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+  </Head>
+  <div>
+    <div>
+      <main></main>
+    </div>
+  </div>
+</template>
